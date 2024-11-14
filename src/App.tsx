7a5b0a0080b8ba7bc7d9017c64f657be153e9c9f
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-11-13 21:52:51 -0700

    shadcn vite starter

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <main className="flex items-center justify-center w-full h-full bg-background">
+      <Button>Click me</Button>
+    </main>
+  );
+}
+
+export default App;
